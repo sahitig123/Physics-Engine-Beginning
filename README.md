e020cbcd35c3ed3540cdb45cb03e73df8f5bae71
sahitig123 <89743954+sahitig123@users.noreply.github.com>
2021-11-20 12:54:55 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Physics-Engine-Beginning
+# C20-Student-boilerplate-code
---
+++ sketch.js
@@ -0,0 +1,58 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+let engine;
+let world;
+var ball;
+var ground;
+var ground1;
+var ball1;
+
+function setup() {
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+  
+   var ball_options = {
+    restitution: 0.95,
+    frictionAir:0.01
+  }
+   
+   var ground_options ={
+     isStatic: true
+   };
+  
+
+  ground = Bodies.rectangle(200,390,400,20,ground_options);
+  World.add(world,ground);
+
+  ball = Bodies.circle(100,10,20,ball_options);
+  World.add(world,ball);
+
+  ball1 = Bodies.circle(300,10,20,ball_options);
+  World.add(world,ball1);
+
+  ground1 = Bodies.rectangle(300,200,200,5,ground_options);
+  World.add(world,ground1);
+  
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+
+function draw() 
+{
+  background(51);
+  Engine.update(engine);
+  
+  ellipse(ball.position.x,ball.position.y,20);
+  rect(ground.position.x,ground.position.y,400,20);
+  ellipse(ball1.position.x,ball1.position.y,20);
+  rect(ground1.position.x,ground1.position.y,200,5);
+ 
+}
+
